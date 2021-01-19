6f64a59bf59144522b5a7f8e6dd437ed1dbc383f
sapir <sapir.madmon@s.afeka.ac.il>
2021-01-19 22:56:51 +0200

    opening project

---
+++ src/main/java/com/example/demo/CustomerGroupShoppingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerGroupShoppingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerGroupShoppingServiceApplication.class, args);
+	}
+
+}
